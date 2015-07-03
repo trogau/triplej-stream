4ac430d216bef9b4e03a3d543da6bc3f4d31d167
David Harrison <trogau@users.noreply.github.com>
2015-07-03 10:18:50 +1000

    Create triplej.bat

---
+++ triplej.bat
@@ -0,0 +1,3 @@
+REM Place this file in your PATH somewhere. 
+REM If the playlist file (triplej.pls) is in your Documents folder, try using %userprofile%\Documents\triplej.pls
+@start c:\temp\triplej.pls
